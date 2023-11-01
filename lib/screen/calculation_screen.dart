925c90c00a662c102b42434fadb94541acb9e91b
MukeshSheoran <mukeshsheoran883@gmail.com>
2023-11-01 14:31:26 +0530

    Container commit

--- lib/screen/calculation_screen.dart
+++ lib/screen/calculation_screen.dart
@@ -9,6 +9,7 @@ class CalculationScreen extends StatefulWidget {
 
 class _CalculationScreenState extends State<CalculationScreen> {
   double _currentSliderValue = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -114,8 +115,8 @@ class _CalculationScreenState extends State<CalculationScreen> {
                         ],
                       ),
                       Slider(
-                        activeColor: Colors.white,
-                        thumbColor: Colors.red,
+                        // activeColor: Colors.white,
+                        thumbColor: Colors.green,
                         inactiveColor: Colors.red,
                         mouseCursor: MaterialStateMouseCursor.clickable,
                         value: _currentSliderValue,
@@ -123,13 +124,140 @@ class _CalculationScreenState extends State<CalculationScreen> {
                         divisions: 1000,
                         label: _currentSliderValue.round().toString(),
                         onChanged: (double value) {
-                          setState(() {
-                            _currentSliderValue = value;
-                          });
+                          setState(
+                            () {
+                              _currentSliderValue = value;
+                            },
+                          );
                         },
                       ),
                     ],
                   ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF484149),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "WEIGHT",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          const Text(
+                            "60",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.2,
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF484149),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "AGE",
+                            style: TextStyle(color: Colors.white, fontSize: 30),
+                          ),
+                          const Text(
+                            "28",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.remove,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Center(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 35,
+                                  ),
+                                ),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color(0xFF615664),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
